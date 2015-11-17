caadf6ed53c06a17644dbe29a346c8040d07876f
ckhrysze <github@ckhrysze.net>
2015-11-16 22:39:54 -0800

    Done with chapter 5

---
+++ chapter5/functions5.exs
@@ -0,0 +1,13 @@
+# NOTE: IO inspect over puts to see actual values
+
+orig1 = Enum.map [1,2,3,4], fn x -> x + 2 end
+IO.inspect orig1
+
+Enum.each [1,2,3,4], fn x -> IO.inspect x end
+
+rewrite1 = Enum.map [1,2,3,4], &(&1 + 2)
+IO.inspect rewrite1
+
+Enum.each [1,2,3,4], &IO.inspect(&1)
+
+IO.puts "Done"
