3302d3eb942caa890120bee84ebfff3dc3c6654e
Chris Hildebrand <childebrand@ballytech.com>
2015-12-08 10:21:44 -0800

    Remove beam file, added 'max' exercise

--- chapter7/my_list.exs
+++ chapter7/my_list.exs
@@ -1,4 +1,5 @@
 defmodule MyList do
+
   # ListsAndRecursion-0
   def sum([]), do: 0
   def sum([head|tail]), do: head+sum(tail)
@@ -8,4 +9,16 @@ defmodule MyList do
   def mapsum([head|tail], func) do
     func.(head) + mapsum(tail, func)
   end
+
+	# ListsAndRecursion-2
+	def max([]), do: 0
+	def max([head | tail]), do: _max(tail, head)
+
+	defp _max([], high), do: high
+	defp _max([head | tail], high) when head > high do
+		_max(tail, head)
+	end
+	defp _max([_head | tail], high) do
+		_max(tail, high)
+	end
 end
